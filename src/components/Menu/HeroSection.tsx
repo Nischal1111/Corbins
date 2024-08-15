58de73edd6ca2e008e4aab7b6bf3e008e0ac3438
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2024-08-15 23:02:54 +0545

    Corbins

--- src/components/Menu/HeroSection.tsx
+++ src/components/Menu/HeroSection.tsx
@@ -9,15 +9,16 @@ const HeroSection = () => {
     <div className={`${subFont.className} w-full flex justify-center flex-col`}>
         <div className='flex flex-col justify-center items-center'>
             <div className='mt-5 flex items-center gap-5'>
-                <h1 className='text-2xl'>Menus</h1>
-                <div className='w-20 h-[1px] bg-gray-400'></div>
+                <div className='lg:w-20 lg:hidden flex w-8 h-[1px] bg-gray-400'></div>
+                <h1 className='lg:text-2xl text-xl'>Menus</h1>
+                <div className='lg:w-20 w-8 h-[1px] bg-gray-400'></div>
             </div>
         </div>
-        <div className='lg:h-[90vh] h-[60vh] w-full rounded-lg mt-8 custom-menu-bg relative'>
-            <div className={`absolute lg:right-[5%] right-0 ${mainFont.className} lg:w-2/5 w-full lg:top-1/4 top-12`}>
+        <div className='lg:h-[90vh] h-[60vh] w-full rounded-lg mt-8 custom-menu-bg relative px-4 lg;px-0'>
+            <div className={`absolute lg:right-[5%] right-0 ${mainFont.className} lg:w-2/5 w-full lg:top-1/4 top-[25%] px-6 lg;px-0`}>
                 <h1 className='lg:text-4xl text-2xl font-semibold tracking-widest'>Our Menu: Culinary Delights Await.</h1>
                 <p className='lg:text-lg text-sm font-extralight lg:leading-7 leading-5 tracking-wide lg:mt-6 mt-3 text-justify'>Explore a curated selection of exquisite dishes, each crafted to tantalize your taste buds. Discover your next favorite meal with our diverse and delicious menu offerings.</p>
-                <button className='bg-gray-300 text-black rounded-sm py-3 px-6 mt-8 tracking-widest text-sm hover:bg-gray-400 hover:text-white duration-500'>See menu below</button>
+                <button className='bg-gray-300 text-black rounded-sm lg:py-3 lg:px-6 px-4 py-2 mt-8 lg:tracking-widest tracking-wide text-sm hover:bg-gray-400 hover:text-white duration-500'>See menu below</button>
             </div>
         </div>
     </div>
--- src/components/Menu/MainSection.tsx
+++ src/components/Menu/MainSection.tsx
@@ -4,19 +4,19 @@ import { mainFont } from '@/app/layout'
 
 const MainSection = () => {
   return (
-    <main className={`my-4 justify-center flex flex-col items-center ${mainFont.className}`}>
+    <main className={`my-4 mb-20 justify-center flex flex-col items-center ${mainFont.className}`}>
 
-        <section className='flex w-[90%] relative mt-[6rem] mb-[4rem] h-[50vh] items-center'>
-            <main className='w-[60%]'>
+        <section className='flex w-[90%] lg:flex-row flex-col relative mt-[6rem] mb-[4rem] h-[50vh] items-center'>
+            <main className='lg:w-[60%] w-full'>
                 <Image src="/assets/menus/food-menu.avif" alt='menu' height={1000} width={1000} className='w-full h-full object-cover'/>
             </main>
-            <main className=' relative w-[50%] -left-[12%] '>
+            <main className=' relative lg:w-[50%] w-full lg:top-0 top-[-20%]  lg:-left-[12%] left-0 '>
                 <div className='bg-[rgba(0,0,0,.7)] p-4 border-t border-r border-gray-400'>
                     <div className='mt-3 flex items-center gap-5'>
-                        <h1 className='text-xl text-white pl-4'>Food Menu</h1>
+                        <h1 className='lg:text-xl text-lg text-white pl-4'>Food Menu</h1>
                         <div className='w-20 h-[1px] bg-white'></div>
                     </div>
-                    <p className='px-4 text-sm leading-6 tracking-wider text-justify mt-4'>
+                    <p className='px-4 lg:text-sm text-xs leading-6 tracking-wider text-justify mt-4'>
                         Embark on a culinary adventure with our meticulously curated food menu. We offer an array of dishes, each crafted with the finest ingredients and a passion for excellence. From savory appetizers and hearty main courses to decadent desserts, our menu caters to all tastes and preferences. Indulge in our signature creations or explore new and exciting flavors. Every dish is designed to provide a unique and unforgettable dining experience, making each visit a celebration of taste and culinary artistry.
                     </p>
                     <button className='bg-zinc-300 text-black rounded-sm py-2 px-4 mt-8 ml-4 tracking-widest text-xs hover:bg-gray-400 duration-500'>View Menu</button>
@@ -25,17 +25,17 @@ const MainSection = () => {
             </main>
         </section>
 
-        <section className='flex w-[90%] relative mt-[5rem] mb-[4rem] h-[50vh] items-center flex-row-reverse'>
-            <main className='w-[60%]'>
+        <section className='flex w-[90%] relative mt-[5rem] mb-[4rem] h-[50vh] items-center lg:flex-row-reverse flex-col'>
+            <main className='lg:w-[60%] w-full'>
                 <Image src="/assets/menus/drink-menu.avif" alt='menu' height={1000} width={1000} className='w-full h-full object-cover'/>
             </main>
-            <main className=' relative w-[50%] -right-[12%]'>
+            <main className=' relative lg:w-[50%] w-full lg:top-0 top-[-20%]  lg:-right-[12%] right-0 '>
                 <div className='bg-[rgba(0,0,0,.7)] p-4 border-b border-l border-gray-400'>
                     <div className='mt-3 flex items-center gap-5'>
-                        <h1 className='text-xl text-white pl-4'>Drink Menu</h1>
+                        <h1 className='lg:text-xl text-lg text-white pl-4'>Drink Menu</h1>
                         <div className='w-20 h-[1px] bg-white'></div>
                     </div>
-                    <p className='px-4 text-sm leading-6 tracking-wider text-justify mt-4'>
+                    <p className='px-4 lg:text-sm text-xs leading-6 tracking-wider text-justify mt-4'>
                         Discover a world of refreshing indulgence with our expertly crafted drink menu. Whether you’re in the mood for a classic cocktail, a robust glass of wine, or a unique handcrafted beverage, our selection offers something for every palate. Savor the flavors of our signature drinks, meticulously prepared with the finest ingredients and a touch of creativity. From invigorating non-alcoholic options to exquisite spirits, our drink menu promises a delightful experience with every sip. Join us in toasting to good times and great flavors.
                     </p>
                     <button className='bg-zinc-300 text-black rounded-sm py-2 px-4 mt-8 ml-4 tracking-widest text-xs hover:bg-gray-400 duration-500'>View Menu</button>
@@ -44,17 +44,17 @@ const MainSection = () => {
             </main>
         </section>
 
-        <section className='flex w-[90%] relative mt-[5rem] mb-[5rem] h-[50vh] items-center'>
-            <main className='w-[60%]'>
+        <section className='flex w-[90%] lg:flex-row flex-col relative mt-[6rem] mb-[4rem] h-[50vh] items-center '>
+            <main className='lg:w-[60%] w-full'>
                 <Image src="/assets/menus/degustation-foods.avif" alt='menu' height={1000} width={1000} className='w-full h-full object-cover'/>
             </main>
-            <main className=' relative w-[50%] -left-[12%]'>
+            <main className='relative lg:w-[50%] w-full lg:top-0 top-[-20%]  lg:-left-[12%] left-0 '>
                 <div className='bg-[rgba(0,0,0,.7)] p-4 border-y border-gray-400'>
                     <div className='mt-3 flex items-center gap-5'>
-                        <h1 className='text-xl text-white pl-4'>Degustation Menu</h1>
+                        <h1 className='lg:text-xl text-lg text-white pl-4'>Degustation Menu</h1>
                         <div className='w-20 h-[1px] bg-white'></div>
                     </div>
-                    <p className='px-4 text-sm leading-6 tracking-wider text-justify mt-4'>
+                    <p className='px-4 lg:text-sm text-xs leading-6 tracking-wider text-justify mt-4'>
                         Experience the pinnacle of culinary artistry with our degustation menu, a carefully curated tasting journey that showcases the finest flavors and innovative techniques. Each course is a masterpiece, designed to complement and enhance the next, offering a harmonious progression of taste sensations. From the freshest ingredients to the most exquisite presentations, our degustation menu is a celebration of creativity and craftsmanship. Immerse yourself in this gourmet adventure, where every bite tells a story and every dish is a revelation.
                     </p>
                     <button className='bg-zinc-300 text-black rounded-sm py-2 px-4 mt-8 ml-4 tracking-widest text-xs hover:bg-gray-400 duration-500'>View Menu</button>
