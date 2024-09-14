7fedb50aba4eee0452e522805fccd622675002e8
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2024-09-14 10:10:10 +0545

    handle Email

--- src/shared/Footer/index.tsx
+++ src/shared/Footer/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import {Divider} from "@nextui-org/divider";
 import { IoCall, IoLocation } from 'react-icons/io5';
 import { mainFont, subFont } from '@/app/layout';
@@ -11,6 +11,15 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '@/motion/motion';
 
 const index = () => {
+  const [email,setEmail]=useState("")
+
+  const handleEmailChange=(e:ChangeEvent<HTMLInputElement>)=>{
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+  const handleEmail=()=>{
+    setEmail("")
+  }
   return (
     <motion.main initial="hidden"
                         whileInView="show"
@@ -51,8 +60,8 @@ const index = () => {
           <p className='lg:text-center text-justify text-sm lg:w-3/5 w-full'>Sign up to our mailing list to stay in the loop with new releases, events and exclusive offers.</p>
 
           <div className='lg:w-3/5 w-full flex gap-4 items-center'>
-            <input type="text" placeholder='Email Address' className=' h-10 border border-gray-400 rounded-md pl-4 w-4/5 focus:outline-none text-black'/>
-            <button className='bg-transparent color-zinc-400 hover:underline lg-text-base text-sm'>Sign Up</button>
+            <input type="text" placeholder='Email Address' value={email} onChange={(e)=>handleEmailChange(e)} className=' h-10 border border-gray-400 rounded-md pl-4 w-4/5 focus:outline-none text-black'/>
+            <button className='bg-transparent color-zinc-400 hover:underline lg-text-base text-sm' onClick={handleEmail}>Sign Up</button>
           </div>
 
           <div className='lg:flex lg:flex-col hidden gap-4 mt-6 lg:items-center items-start justify-center  '>
